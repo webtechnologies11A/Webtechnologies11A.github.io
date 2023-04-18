d45ab5d9094c9222653e9580446049ac1c610d82
webtechnologies11A <131159830+webtechnologies11A@users.noreply.github.com>
2023-04-18 14:12:07 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let day = "вівторок"
+
+alert("Це перша сторінка сайта")
+
+alert("jhg 2"/0)
+alert(`Сьогодні ${day}`)
+
+
+let result = prompt("Яке сьогодні число?","14.03.2023" )
+
+alert(`Було введено: ${result}`)
+let result = confirm ("Сьогодні точно вівторок?")
+alert (result)
+
+let a = prompt ("Введіть число")
+
+alert(`Добуток числа ${a} і 10 дорівнює ${a*10}`)
